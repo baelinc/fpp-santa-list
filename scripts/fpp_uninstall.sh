c7a095482e7361a278ac5321d811906b167b9794
baelinc <11509732+baelinc@users.noreply.github.com>
2025-12-18 13:29:41 -0600

    Update fpp_uninstall.sh

--- scripts/fpp_uninstall.sh
+++ scripts/fpp_uninstall.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-echo "Stopping Santa List background worker..."
+echo "Stopping Santa Worker..."
 sudo pkill -f santa_worker.py
 
-echo "Clearing LED Matrix screens..."
-/opt/fpp/bin/fppmm -m Screen1 -o off
-/opt/fpp/bin/fppmm -m Screen2 -o off
-
-echo "Removing configuration files..."
+echo "Cleaning up..."
 rm -f /home/fpp/media/config/plugin.fpp-santa-list.json
 
-echo "Santa List Plugin uninstalled successfully."
+echo "Uninstall complete."
