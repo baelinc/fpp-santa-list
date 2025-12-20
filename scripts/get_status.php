fe626efbc21dcecaec3f6ffbd9d030de02c8e944
baelinc <11509732+baelinc@users.noreply.github.com>
2025-12-20 12:47:50 -0600

    Update get_status.php

--- scripts/get_status.php
+++ scripts/get_status.php
@@ -1,12 +1,21 @@
 <?php
 header('Content-Type: application/json');
 
-// Use 'pgrep' which is much more reliable than 'ps | grep' 
-// because it won't return the grep process itself.
-exec("pgrep -f fpp_santa_worker.php", $output);
+// pgrep -f looks for the pattern in the full command line
+// We use 'pgrep' because 'ps | grep' often catches itself, 
+// causing the "Always Running" bug you are seeing.
+exec("pgrep -f fpp_santa_worker.php", $pids);
 
-// If the array output has any PIDs, it is running
-$isRunning = (count($output) > 0);
+// Filter out any potential empty strings and count
+$isRunning = false;
+if (!empty($pids)) {
+    foreach ($pids as $pid) {
+        if (is_numeric(trim($pid))) {
+            $isRunning = true;
+            break;
+        }
+    }
+}
 
 echo json_encode(['running' => $isRunning]);
 ?>
