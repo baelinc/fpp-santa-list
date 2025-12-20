9b9986ab4d6af9a0a6145dd7dd6c735f995708da
baelinc <11509732+baelinc@users.noreply.github.com>
2025-12-20 12:43:35 -0600

    Update get_status.php

--- scripts/get_status.php
+++ scripts/get_status.php
@@ -1,8 +1,11 @@
 <?php
 header('Content-Type: application/json');
-// Search for the santa_worker.php process
-exec("ps aux | grep santa_worker.php | grep -v grep", $output);
 
+// Use 'pgrep' which is much more reliable than 'ps | grep' 
+// because it won't return the grep process itself.
+exec("pgrep -f fpp_santa_worker.php", $output);
+
+// If the array output has any PIDs, it is running
 $isRunning = (count($output) > 0);
 
 echo json_encode(['running' => $isRunning]);
