e3a4611ba33404b6937bd68d40fad6fc7b14e4fa
baelinc <11509732+baelinc@users.noreply.github.com>
2025-12-19 23:25:51 -0600

    Update plugin_setup.php

--- plugin_setup.php
+++ plugin_setup.php
@@ -1,18 +1,32 @@
-<div id="santa_settings" class="settings">
+<?php
+include_once 'common.php';
+$pluginName = "fpp-santa-list";
+// Fetch saved settings or use defaults
+$wp_url = isset($pluginSettings['wp_url']) ? $pluginSettings['wp_url'] : "";
+$name_limit = isset($pluginSettings['name_limit']) ? $pluginSettings['name_limit'] : "6";
+$header_font = isset($pluginSettings['header_font']) ? $pluginSettings['header_font'] : "18";
+$names_font = isset($pluginSettings['names_font']) ? $pluginSettings['names_font'] : "12";
+?>
+<div id="santa_list" class="settings">
     <fieldset>
-        <legend>Santa Matrix Settings</legend>
-        <p>WordPress API URL: <input type='text' id='wp_url' size='40' value='<?php echo $pluginSettings['wp_url']; ?>'></p>
+        <legend>Santa's List Configuration</legend>
+        <table cellspacing="5" cellpadding="5">
+            <tr><td>WordPress API URL:</td><td><input type="text" id="wp_url" size="60" value="<?php echo $wp_url; ?>"></td></tr>
+            <tr><td>Names to Show:</td><td><input type="number" id="name_limit" value="<?php echo $name_limit; ?>"></td></tr>
+        </table>
         <hr>
-        <h3>Top Screen (Header)</h3>
-        <p>Model Name: <input type='text' id='header_model' value='Matrix_Header'></p>
-        <p>Font Size: <input type='number' id='header_font' value='18'></p>
-        <p>Nice Color: <input type='color' id='nice_color' value='#00FF00'></p>
-        <p>Naughty Color: <input type='color' id='naughty_color' value='#FF0000'></p>
+        <h3>Matrix Overlay Models</h3>
+        <table cellspacing="5" cellpadding="5">
+            <tr><td>Header Model (Top):</td><td><input type="text" id="header_model" value="Matrix_Header"></td></tr>
+            <tr><td>Names Model (Bottom):</td><td><input type="text" id="names_model" value="Matrix_Names"></td></tr>
+        </table>
         <hr>
-        <h3>Bottom Screen (Names)</h3>
-        <p>Model Name: <input type='text' id='names_model' value='Matrix_Names'></p>
-        <p>Font Size: <input type='number' id='names_font' value='12'></p>
-        <p>Text Color: <input type='color' id='text_color' value='#FFFFFF'></p>
-        <p>Names to Show: <input type='number' id='name_limit' value='6'></p>
+        <h3>Appearance</h3>
+        <table cellspacing="5" cellpadding="5">
+            <tr><td>Header Font Size:</td><td><input type="number" id="header_font" value="<?php echo $header_font; ?>"></td></tr>
+            <tr><td>Names Font Size:</td><td><input type="number" id="names_font" value="<?php echo $names_font; ?>"></td></tr>
+            <tr><td>Nice List Color:</td><td><input type="color" id="nice_color" value="#00FF00"></td></tr>
+            <tr><td>Naughty List Color:</td><td><input type="color" id="naughty_color" value="#FF0000"></td></tr>
+        </table>
     </fieldset>
 </div>
